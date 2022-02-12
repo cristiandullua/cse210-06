f972459b30d19a4c2cf66db890d1aaed3c2470c6
Cristian D Ullua <cris.ullua.95@gmail.com>
2022-02-12 13:52:11 -0300

    end game at the right point

--- game/parachute.py
+++ game/parachute.py
@@ -2,37 +2,45 @@ from email import message
 
 class Parachute:
     def __init__(self):
+        self._alive = True
         self._counter = 0
-        self._parachute = [' ___ ',
-                          '/___\\',
-                          '\\   /',
-                          ' \\ /',
-                          '  0',
-                          ' /|\\',
-                          ' / \\'
+        self._parachute = ['  ___ ',
+                          ' /___\\',
+                          ' \\   /',
+                          '  \\ /',
+                          '   0',
+                          '  /|\\',
+                          '  / \\'
                           ]
         
     # Delete first line of the parachute
     def delete_line(self):
         self._parachute.pop(0)
+
+        if '0' in self._parachute[0]:
+            self._alive = False
+            self._parachute[0] = self._parachute[0].replace('0', 'x')
+
         self._counter += 1
-    
+        
     def get_hint(self):
-        hint = " "
+        hint = ''
         if  self._counter > 0 and self._counter < 2: 
-            hint = "Ups! I looks like your parachute is in trouble!"
+            hint = 'Ups! It looks like your parachute is in trouble!'
         elif  self._counter > 2 and self._counter < 4: 
-            hint = "Be carefull jumper, you can lose your parachute!" 
+            hint = 'Be carefull jumper, you can lose your parachute!' 
         elif  self._counter == 4: 
-            hint = "No parachute. You lose!"
+            hint = 'No parachute. You lose!'
         return hint  
-                           
+
     def lost_parachute(self):
         return self._counter == 5 
     
     # Print parachute
     def display(self):
-        print()
         for i in self._parachute:
             print(i)
         print('^^^^^^^')
+    
+    def is_alive(self):
+        return self._alive
